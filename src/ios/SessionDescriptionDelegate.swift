fc27706f387780a02879f67b34fefa25430be030
ningsuhen <ningsuhen@gmail.com>
2015-10-29 18:34:00 +0530

    replace println with print for swift 2

--- src/ios/SessionDescriptionDelegate.swift
+++ src/ios/SessionDescriptionDelegate.swift
@@ -2,52 +2,52 @@ import Foundation
 
 class SessionDescriptionDelegate : UIResponder, RTCSessionDescriptionDelegate {
     var session: Session
-    
+
     init(session: Session) {
         self.session = session
     }
-    
+
     func peerConnection(peerConnection: RTCPeerConnection!,
         didCreateSessionDescription originalSdp: RTCSessionDescription!, error: NSError!) {
         if error != nil {
-            println("SDP OnFailure: \(error)")
+            print("SDP OnFailure: \(error)")
             return
         }
-            
+
         var sdp = RTCSessionDescription(
             type: originalSdp.type,
             sdp: self.session.preferISAC(originalSdp.description)
         )
-        
+
         self.session.peerConnection.setLocalDescriptionWithDelegate(self, sessionDescription: sdp)
-            
+
         dispatch_async(dispatch_get_main_queue()) {
             var jsonError: NSError?
-            
+
             let json: AnyObject = [
                 "type": sdp.type,
                 "sdp": sdp.description
             ]
-            
+
             let data = NSJSONSerialization.dataWithJSONObject(json,
                 options: NSJSONWritingOptions.allZeros,
                 error: &jsonError)
-            
+
             self.session.sendMessage(data!)
         }
     }
-    
+
     func peerConnection(peerConnection: RTCPeerConnection!,
         didSetSessionDescriptionWithError error: NSError!) {
         if error != nil {
-            println("SDP OnFailure: \(error)")
+            print("SDP OnFailure: \(error)")
             return
         }
-            
+
         dispatch_async(dispatch_get_main_queue()) {
             if self.session.config.isInitiator {
                 if self.session.peerConnection.remoteDescription != nil {
-                    println("SDP onSuccess - drain candidates")
+                    print("SDP onSuccess - drain candidates")
                     self.drainRemoteCandidates()
                 }
             } else {
@@ -60,7 +60,7 @@ class SessionDescriptionDelegate : UIResponder, RTCSessionDescriptionDelegate {
             }
         }
     }
-    
+
     func drainRemoteCandidates() {
         if self.session.queuedRemoteCandidates != nil {
             for candidate in self.session.queuedRemoteCandidates! {
